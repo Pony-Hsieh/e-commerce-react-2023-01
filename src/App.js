ecf8271645911e92acd889f8ec236101bdeadaa2
Pony Hsieh <lbjwin@gmail.com>
2023-03-06 20:23:29 +0800

    feat: 新增 404 頁面及相對應 route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import SingleProduct from './pages/SingleProduct';
 import Cart from './pages/Cart';
 import SingleOrder from './pages/SingleOrder';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path={'/cart'} element={<Cart />}></Route>
         <Route path={'/singleOrder'} element={<SingleOrder />}></Route>
         <Route path={'/singleProduct'} element={<SingleProduct />}></Route>
+        <Route path={'*'} element={<NoMatch />}></Route>
       </Routes>
     </>
   );
---
+++ src/pages/NoMatch.js
@@ -0,0 +1,9 @@
+function NoMatch() {
+  return (
+    <>
+      <h1>找不到相對應的頁面</h1>
+    </>
+  );
+}
+
+export default NoMatch;
