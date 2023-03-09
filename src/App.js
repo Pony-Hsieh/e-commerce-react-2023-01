c59a87b2d49d0e083cb12fe57316b5bea67a17a1
Pony Hsieh <lbjwin@gmail.com>
2023-03-09 18:29:28 +0800

    feat: 新增 mui spacing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import Cart from './pages/Cart';
 import SingleOrder from './pages/SingleOrder';
 import NoMatch from './pages/NoMatch';
 import CssBaseline from '@mui/material/CssBaseline'; // css normalize
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  spacing: 8,
+});
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Routes>
-        <Route path={'/'} element={<Home />} ></Route>
-        <Route path={'/cart'} element={<Cart />}></Route>
-        <Route path={'/singleOrder'} element={<SingleOrder />}></Route>
-        <Route path={'/singleProduct'} element={<SingleProduct />}></Route>
-        <Route path={'*'} element={<NoMatch />}></Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path={'/'} element={<Home />}></Route>
+          <Route path={'/cart'} element={<Cart />}></Route>
+          <Route path={'/singleOrder'} element={<SingleOrder />}></Route>
+          <Route path={'/singleProduct'} element={<SingleProduct />}></Route>
+          <Route path={'*'} element={<NoMatch />}></Route>
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
