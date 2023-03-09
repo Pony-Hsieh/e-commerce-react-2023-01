3e7df2ddfdeaad2e6bfb23f60d3bdc3f7509c7ce
Pony Hsieh <lbjwin@gmail.com>
2023-03-09 18:13:20 +0800

    refactor: 移除用不到的模板創建內容

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../slices/counter/counterSlice';
 import localStorageCartReducer from '../slices/localStorageCartSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     localStorageCart: localStorageCartReducer,
   },
   middleware: (getDefaultMiddleware) =>
