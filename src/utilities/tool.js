ca808ae3b2f13eb7404f2a6a3ff3028aa5325933
Pony Hsieh <lbjwin@gmail.com>
2023-03-09 18:25:44 +0800

    feat: 新增共用方法 createIdByTime
    產出 id

--- src/utilities/tool.js
+++ src/utilities/tool.js
@@ -1,4 +1,14 @@
 /**
+ * 產出 id
+ * @return {string}
+ */
+export function createIdByTime() {
+  const dateTime = Date.now();
+  const perform = performance.now().toFixed(0);
+  return `${dateTime}${perform}`;
+}
+
+/**
  * 透過網頁寬度判斷裝置
  * 
  * 參考 mui v5 的 Breakpoints
