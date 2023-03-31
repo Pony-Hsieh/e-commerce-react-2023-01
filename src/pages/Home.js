1374ef9bab3559ce932a8f8c32b0e7cc7f6dde24
Pony Hsieh <lbjwin@gmail.com>
2023-03-31 19:15:49 +0800

    feat: 更新首頁樣式

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,36 @@
+import NavBar from "../components/NavBar";
+import {
+  Box,
+  Typography,
+} from '@mui/material';
+import bannerImage from '../images/banner.jpeg';
+
 function Home() {
   return (
     <>
-      <h1>Home 首頁</h1>
+      <Box sx={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        color: "#f8ffff",
+        backgroundImage: `url(${bannerImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}>
+        <NavBar />
+        <Typography component="h3"
+          variant="h2"
+          sx={(theme) => ({
+            position: "absolute",
+            bottom: theme.spacing(2),
+            left: theme.spacing(2),
+          })}
+        >
+          少，<br />
+          但是更好
+        </Typography>
+      </Box>
     </>
   );
 }
